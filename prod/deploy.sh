f984465850235fe6299f5fc92f1d488cc7e4c6c0
Alexei Gontar <f.gontar5@gmail.com>
2021-05-09 10:05:57 +0300

    #8: Setup deploy and https for frontend

---
+++ prod/deploy.sh
@@ -0,0 +1,20 @@
+#/usr/vin/env bash
+
+if [[ -f ".env" ]]; then
+  echo "Exporting .env variables"
+  export $(grep -v '^#' .env | xargs)
+fi
+
+services=(db frontend server)
+
+for service in ${services[*]}
+do
+  echo "Service: $service"
+  pushd "../$service";
+  docker-compose build;
+  docker-compose push;
+  popd;
+done
+
+ssh -i "$SERVER_PEM_LOCATION" "ec2-user@$SERVER_HOST" "cd ~/confin && docker-compose pull"
+ssh -i "$SERVER_PEM_LOCATION" "ec2-user@$SERVER_HOST" "cd ~/confin && docker-compose up -d"
